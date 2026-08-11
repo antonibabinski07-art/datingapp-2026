0a6fcee56a28348a55949ae0abb2994f775f5470
antoniusz <antonibabinski07@gmail.com>
2026-08-11 10:52:53 +0200

    End of section 8

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,29 +1,31 @@
-using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 [Authorize]
-public class MembersController(AppDbContext context) : ApiBaseController
+public class MembersController(IMemberRepository memberRepository) : ApiBaseController
 {
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
     {
-        var members = await context.Users.ToListAsync();
-
-        return members;
+        return Ok(await memberRepository.GetMembersAsync());
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetMember(string id)
+    public async Task<ActionResult<Member>> GetMember(string id)
     {
-        var member = await context.Users.FindAsync(id);
+        var member = await memberRepository.GetMemberByIdAsync(id);
         if(member == null) return NotFound();
 
         return member;
     }
 
+    [HttpGet("{id}/photos")]
+    public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
+    {
+        return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+    }
 }
