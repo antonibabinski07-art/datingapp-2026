79fad46c29d309508517c75c425df677c7f52327
antoniusz <antonibabinski07@gmail.com>
2026-08-17 13:42:07 +0200

    End of section 9

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,21 +1,42 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Location, NgClass } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 import { AccountService } from '../../core/services/account-service';
 import { ToastService } from '../../core/services/toast-service';
+import { themes } from '../../data/themes';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, RouterLink, RouterLinkActive],
+  imports: [FormsModule, RouterLink, RouterLinkActive, NgClass],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {
-  protected readonly accountService = inject(AccountService);
-  private readonly router = inject(Router);
-  private readonly toastService = inject(ToastService);
+export class Nav implements OnInit {
+  private router = inject(Router);
+  private location = inject(Location)
+  protected accountService = inject(AccountService);
+  private toastService = inject(ToastService);
   protected creds: any = {};
+  protected themes = themes;
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+
+  ngOnInit(): void {
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
+
+  handleSetTheme(theme: string): void {
+    if(!themes.includes(theme)) return;
+
+    if(theme !== this.selectedTheme()) {
+      localStorage.setItem('theme', theme);
+      this.selectedTheme.set(theme);
+      document.documentElement.setAttribute('data-theme', theme);
+      const el = document.activeElement as HTMLDivElement;
+      if(el) el.blur();
+    }
+  }
 
   login() {
     this.accountService.login(this.creds).subscribe({
@@ -32,5 +53,9 @@ export class Nav {
     this.accountService.logout();
     this.router.navigateByUrl('/');
     this.toastService.info("Logged out", 2000);
+  }
+
+  isRootRoute(): boolean {
+    return this.router.url === '/';
   }
 }
